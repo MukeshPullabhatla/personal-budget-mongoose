feb21d1d7ca528eb88e8846144387f46fad3548a
MukeshPullabhatla <mpullabh@uncc.edu>
2020-10-30 22:01:10 -0400

    added mongoose and mongo

---
+++ server.js
@@ -0,0 +1,30 @@
+const express=require('express');
+const cors=require('cors');
+const mongoose=require("mongoose");
+const routes=require("./routes");
+const app=express();
+const port =3000;
+var url = "mongodb://localhost:27017/personal_budget";
+var budgetModel=require('./models/budgetData');
+const budget = require("./budget");
+const router = express.Router()
+
+
+mongoose.connect(url,{useNewUrlParser:true, useUnifiedTopology:true})
+        .then(()=>{
+            app.use(express.json())
+            app.use("/api",routes)
+        })
+        .catch((connectionError)=>{
+          console.log(connectionError)
+        })
+
+app.use(cors());
+
+app.get('/budget', (req,res)=>{
+    res.json(budget);
+});
+
+app.listen(port,()=>{
+    console.log(` API Served in this http://localhost:${port}`)
+});
